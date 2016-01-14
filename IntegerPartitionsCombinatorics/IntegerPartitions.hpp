f5e215fe99fe7878e48318c9d6f32db59fa5b5a9
softserve-x10 <softserve.x10@gmail.com>
2016-01-13 23:42:33 -0800

    Created demo abstract integer partitions class with forward iterator.

---
+++ IntegerPartitionsCombinatorics/IntegerPartitions.hpp
@@ -0,0 +1,108 @@
+#ifndef INTEGER_PARTITIONS_COMBINATORICS_INTEGER_PARTITIONS_HPP
+#define INTEGER_PARTITIONS_COMBINATORICS_INTEGER_PARTITIONS_HPP
+
+#include <vector>
+
+////////////////////////////////////////////////////////////////////////
+/// The IntegerPartitionsCombinatorics namespace.
+////////////////////////////////////////////////////////////////////////
+namespace IPC
+{
+    ////////////////////////////////////////////////////////////////////////
+    /// \brief Stores an integer and, abstractly, its partitions.
+    ///
+    /// The partitions of this class's integer are obtainable only via
+    /// a forward iterator.
+    /// To obtain the forward iterator, use the begin method.
+    /// From there, use the operator++ method to advance to the "next"
+    /// partition.
+    /// Each partition is a vector of multiplicities.
+    ////////////////////////////////////////////////////////////////////////
+    /// \todo Make the partition a hash map of multiplicities to save space.
+    ////////////////////////////////////////////////////////////////////////
+    class IntegerPartitions
+    {
+    public:
+        IntegerPartitions(const std::size_t& integer);
+
+        typedef std::vector<std::size_t> Partition;
+
+        std::size_t getInteger() const;
+
+        class Iterator;
+
+        Iterator begin() const;
+        Iterator end() const;
+
+    private:
+        ////////////////////////////////////////////////////////////////////////
+        /// \brief The integer from which the partitions are obtained.
+        ////////////////////////////////////////////////////////////////////////
+        std::size_t integer;
+    };
+
+    ////////////////////////////////////////////////////////////////////////
+    /// \brief The forward iterator for the IntegerPartition class.
+    ///
+    /// Note that the user cannot directly construct this iterator;
+    /// it can only be made via the IntegerPartition class's begin and end methods.
+    ////////////////////////////////////////////////////////////////////////
+    /// \todo Figure out how to make this iterator bidirectional.
+    ////////////////////////////////////////////////////////////////////////
+    class IntegerPartitions::Iterator
+    {
+        friend class IntegerPartitions;
+        friend bool operator==(const Iterator& it1, const Iterator& it2);
+    public:
+        Iterator& operator++();
+        Iterator operator++(int);
+        const IntegerPartitions::Partition operator*() const;
+
+    private:
+        Iterator(const IntegerPartitions * const container);
+
+        ////////////////////////////////////////////////////////////////////////
+        /// \brief The current partition this iterator contains.
+        ///////////////////////////////////////////////////////////////////////////
+        IntegerPartitions::Partition partition;
+        ////////////////////////////////////////////////////////////////////////
+        /// \brief The parent object of this iterator.
+        ////////////////////////////////////////////////////////////////////////
+        const IntegerPartitions* container;
+    };
+
+    ////////////////////////////////////////////////////////////////////////
+    /// \brief Determine if two iterators are equal.
+    /// \param it1 is the LHS iterator to be compared.
+    /// \param it2 is the RHS iterator to be compared.
+    /// \return true if the iterators are equal, false otherwise.
+    ///
+    /// Iterators are equal if they contain the same integer partition,
+    /// and if they belong to the same parent.
+    ////////////////////////////////////////////////////////////////////////
+    inline bool operator==(
+            const IntegerPartitions::Iterator& it1,
+            const IntegerPartitions::Iterator& it2)
+    {
+        return (it1.partition   == it2.partition &&
+                it1.container   == it2.container);
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    /// \brief Determine if two iterators are not equal.
+    /// \param it1 is the LHS iterator to be compared.
+    /// \param it2 is the RHS iterator to be compared.
+    /// \return true if the iterators are equal, false otherwise.
+    ///
+    /// Iterators are not equal if they contain different integer partitions,
+    /// or if they belong to different parents.
+    ////////////////////////////////////////////////////////////////////////
+    inline bool operator!=(
+            const IntegerPartitions::Iterator& it1,
+            const IntegerPartitions::Iterator& it2)
+    {
+        return !(it1 == it2);
+    }
+}
+
+#endif // INTEGER_PARTITIONS_COMBINATORICS_INTEGER_PARTITIONS_HPP
---
+++ IntegerPartitionsCombinatorics/main.cpp
@@ -0,0 +1,109 @@
+////////////////////////////////////////////////////////////////////////
+/// \file main.cpp
+////////////////////////////////////////////////////////////////////////
+/// \mainpage
+/// \author Brenden Case
+///
+/// Here I demonstrate an IntegerPartitions class with a forward iterator.
+////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+
+#include "IntegerPartitions.hpp"
+
+////////////////////////////////////////////////////////////////////////
+/// \brief Outputs an integer partition as a vector of multiplicities to a stream.
+/// \param os is the stream to which the integer partition is output.
+/// \param partition is the integer partition to output.
+/// \return the stream, now containing the integer partition.
+////////////////////////////////////////////////////////////////////////
+std::ostream& operator<<(std::ostream& os, const IPC::IntegerPartitions::Partition& partition)
+{
+    for (auto elnt : partition)
+    {
+        os << elnt << " ";
+    }
+    os.put('\n');
+    os.flush();
+
+    return os;
+}
+
+////////////////////////////////////////////////////////////////////////
+/// \brief The main routine.
+/// \return 0 if the program exits successfully.
+///
+/// Demonstrates the IntegerPartition class and its forward iterator.
+/// The user has an option to view an integer's partitions as vectors
+///  of multiplicities or as Ferrers diagrams in the console.
+/// For large integers, this isn't useful.
+///////////////////////////////////////////////////////////////////////////
+int main()
+{
+    size_t N = 0;
+    do
+    {
+        std::cout << "Enter a positive integer, or 0 to exit: ";
+        std::cin >> N;
+
+        if (N != 0)
+        {
+            IPC::IntegerPartitions ip(N);
+            size_t choice = 0;
+
+            std::cout << "1) Vector of multiplicities.\n"
+                         "2) Ferrers diagram.\n"
+                         "Please enter the number corresponding to how you would like to\n"
+                         "display the partitions: ";
+            std::cin >> choice;
+
+            switch (choice)
+            {
+            case 1:
+            {
+                size_t counter = 0;
+                for (auto part : ip)
+                {
+                    std::cout << part;
+                    ++counter;
+                }
+                std::cout << "There are " << counter << " partitions of " << N << "." << std::endl;
+            }
+                break;
+            case 2:
+            {
+                size_t counter = 0;
+                size_t mult = 0;
+                for (auto part : ip)
+                {
+                    for (size_t i = 1; i <= part.size(); ++i)
+                    {
+                        mult = part.at(i-1);
+                        while (mult > 0)
+                        {
+                            for (size_t j = i; j > 0; --j)
+                                std::cout << '*';
+                            --mult;
+                            std::cout << std::endl;
+                        }
+                    }
+                    std::cout << std::endl;
+                    ++counter;
+                }
+                std::cout << "There are " << counter << " partitions of " << N << "." << std::endl;
+            }
+                break;
+            default:
+            {
+                std::cout << "Invalid input.\n\n";
+            }
+                break;
+            }
+        }
+    } while (N != 0);
+
+    std::cout << "Goodbye!\n";
+
+    return 0;
+}
