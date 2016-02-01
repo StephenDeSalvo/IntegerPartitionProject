41b80fdf16cebe65b20b6fd3f1b33538f8c4775a
softserve-x10 <softserve.x10@gmail.com>
2016-02-01 14:07:49 -0800

    Updated main to demo the id element of the Point class

--- IntegerPartitionsCombinatorics/main.cpp
+++ IntegerPartitionsCombinatorics/main.cpp
@@ -33,7 +33,7 @@ using namespace IPC;
 std::ostream& operator<<(std::ostream& os, const std::vector<Point<int64>>& ps)
 {
     for (auto elnt : ps)
-        os << "(" << elnt.x << "," << elnt.y << ")\n";
+        os << "(" << elnt.x << "," << elnt.y << "), ID: " << elnt.id << "\n";
 
     os.flush();
 
@@ -51,25 +51,25 @@ int main()
 
 //    std::vector<Point<int64>> points;
 
-//    points.push_back(Point<int64>(1,1));
-//    points.push_back(Point<int64>(2,1));
-//    points.push_back(Point<int64>(3,1));
-//    points.push_back(Point<int64>(4,1));
-//    points.push_back(Point<int64>(5,1));
-//    points.push_back(Point<int64>(6,1));
-//    points.push_back(Point<int64>(1,2));
-//    points.push_back(Point<int64>(2,2));
-//    points.push_back(Point<int64>(3,2));
-//    points.push_back(Point<int64>(4,2));
-//    points.push_back(Point<int64>(5,2));
-//    points.push_back(Point<int64>(1,3));
-//    points.push_back(Point<int64>(2,3));
-//    points.push_back(Point<int64>(3,3));
-//    points.push_back(Point<int64>(4,3));
-//    points.push_back(Point<int64>(5,3));
-//    points.push_back(Point<int64>(1,4));
-//    points.push_back(Point<int64>(2,4));
-//    points.push_back(Point<int64>(3,4));
+//    points.push_back(Point<int64>(1,1,1));
+//    points.push_back(Point<int64>(2,1,2));
+//    points.push_back(Point<int64>(3,1,3));
+//    points.push_back(Point<int64>(4,1,4));
+//    points.push_back(Point<int64>(5,1,5));
+//    points.push_back(Point<int64>(6,1,6));
+//    points.push_back(Point<int64>(1,2,7));
+//    points.push_back(Point<int64>(2,2,8));
+//    points.push_back(Point<int64>(3,2,9));
+//    points.push_back(Point<int64>(4,2,10));
+//    points.push_back(Point<int64>(5,2,11));
+//    points.push_back(Point<int64>(1,3,12));
+//    points.push_back(Point<int64>(2,3,13));
+//    points.push_back(Point<int64>(3,3,14));
+//    points.push_back(Point<int64>(4,3,15));
+//    points.push_back(Point<int64>(5,3,16));
+//    points.push_back(Point<int64>(1,4),17);
+//    points.push_back(Point<int64>(2,4),18);
+//    points.push_back(Point<int64>(3,4),19);
 
 //    // Demo the shift BGT.
 //    auto new_pointsShift = BGT::shift<int64>(points, slMatrix);
@@ -114,53 +114,53 @@ int main()
      * Basic Aritmetic Transform Demo
      */
     std::vector<Point<int64>> more_points1;
-    more_points1.push_back(Point<int64>(1,1));
-    more_points1.push_back(Point<int64>(2,1));
-    more_points1.push_back(Point<int64>(3,1));
-    more_points1.push_back(Point<int64>(4,1));
-    more_points1.push_back(Point<int64>(5,1));
-    more_points1.push_back(Point<int64>(1,2));
-    more_points1.push_back(Point<int64>(2,2));
-    more_points1.push_back(Point<int64>(3,2));
-    more_points1.push_back(Point<int64>(4,2));
-    more_points1.push_back(Point<int64>(5,2));
-    more_points1.push_back(Point<int64>(1,3));
-    more_points1.push_back(Point<int64>(2,3));
-    more_points1.push_back(Point<int64>(3,3));
-    more_points1.push_back(Point<int64>(4,3));
-    more_points1.push_back(Point<int64>(1,4));
-    more_points1.push_back(Point<int64>(2,4));
-    more_points1.push_back(Point<int64>(3,4));
-    more_points1.push_back(Point<int64>(4,4));
-    more_points1.push_back(Point<int64>(1,5));
-    more_points1.push_back(Point<int64>(2,5));
-    more_points1.push_back(Point<int64>(1,6));
-    more_points1.push_back(Point<int64>(2,6));
+    more_points1.push_back(Point<int64>(1,1,1));
+    more_points1.push_back(Point<int64>(2,1,2));
+    more_points1.push_back(Point<int64>(3,1,3));
+    more_points1.push_back(Point<int64>(4,1,4));
+    more_points1.push_back(Point<int64>(5,1,5));
+    more_points1.push_back(Point<int64>(1,2,6));
+    more_points1.push_back(Point<int64>(2,2,7));
+    more_points1.push_back(Point<int64>(3,2,8));
+    more_points1.push_back(Point<int64>(4,2,9));
+    more_points1.push_back(Point<int64>(5,2,10));
+    more_points1.push_back(Point<int64>(1,3,11));
+    more_points1.push_back(Point<int64>(2,3,12));
+    more_points1.push_back(Point<int64>(3,3,13));
+    more_points1.push_back(Point<int64>(4,3,14));
+    more_points1.push_back(Point<int64>(1,4,15));
+    more_points1.push_back(Point<int64>(2,4,16));
+    more_points1.push_back(Point<int64>(3,4,17));
+    more_points1.push_back(Point<int64>(4,4,18));
+    more_points1.push_back(Point<int64>(1,5,19));
+    more_points1.push_back(Point<int64>(2,5,20));
+    more_points1.push_back(Point<int64>(1,6,21));
+    more_points1.push_back(Point<int64>(2,6,22));
 
     std::vector<Point<int64>> more_points2;
-    more_points2.push_back(Point<int64>(1,1));
-    more_points2.push_back(Point<int64>(2,1));
-    more_points2.push_back(Point<int64>(3,1));
-    more_points2.push_back(Point<int64>(4,1));
-    more_points2.push_back(Point<int64>(5,1));
-    more_points2.push_back(Point<int64>(6,1));
-    more_points2.push_back(Point<int64>(7,1));
-    more_points2.push_back(Point<int64>(1,2));
-    more_points2.push_back(Point<int64>(2,2));
-    more_points2.push_back(Point<int64>(3,2));
-    more_points2.push_back(Point<int64>(4,2));
-    more_points2.push_back(Point<int64>(5,2));
-    more_points2.push_back(Point<int64>(1,3));
-    more_points2.push_back(Point<int64>(2,3));
-    more_points2.push_back(Point<int64>(3,3));
-    more_points2.push_back(Point<int64>(4,3));
-    more_points2.push_back(Point<int64>(5,3));
-    more_points2.push_back(Point<int64>(1,4));
-    more_points2.push_back(Point<int64>(2,4));
-    more_points2.push_back(Point<int64>(1,5));
-    more_points2.push_back(Point<int64>(2,5));
-    more_points2.push_back(Point<int64>(1,6));
-    more_points2.push_back(Point<int64>(1,7));
+    more_points2.push_back(Point<int64>(1,1,23));
+    more_points2.push_back(Point<int64>(2,1,24));
+    more_points2.push_back(Point<int64>(3,1,25));
+    more_points2.push_back(Point<int64>(4,1,26));
+    more_points2.push_back(Point<int64>(5,1,27));
+    more_points2.push_back(Point<int64>(6,1,28));
+    more_points2.push_back(Point<int64>(7,1,29));
+    more_points2.push_back(Point<int64>(1,2,30));
+    more_points2.push_back(Point<int64>(2,2,31));
+    more_points2.push_back(Point<int64>(3,2,32));
+    more_points2.push_back(Point<int64>(4,2,33));
+    more_points2.push_back(Point<int64>(5,2,34));
+    more_points2.push_back(Point<int64>(1,3,35));
+    more_points2.push_back(Point<int64>(2,3,36));
+    more_points2.push_back(Point<int64>(3,3,37));
+    more_points2.push_back(Point<int64>(4,3,38));
+    more_points2.push_back(Point<int64>(5,3,39));
+    more_points2.push_back(Point<int64>(1,4,40));
+    more_points2.push_back(Point<int64>(2,4,41));
+    more_points2.push_back(Point<int64>(1,5,42));
+    more_points2.push_back(Point<int64>(2,5,43));
+    more_points2.push_back(Point<int64>(1,6,44));
+    more_points2.push_back(Point<int64>(1,7,45));
 
 //    // Demo the sum BAT.
 //    auto sumpoints = IPC::BAT::sum(more_points1,more_points2);
